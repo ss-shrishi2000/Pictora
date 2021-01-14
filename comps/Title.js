87186647626af6b491c35dc27d5db5969eb36e5e
Shrishi2000 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-01-14 16:17:52 +0530

    Add files via upload

---
+++ comps/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1> PICTORA </h1>
+      <h3>  By Shrishti Singh!! </h3>
+      <h2> Your Moments Captured!!! </h2>
+      <p> This Is a Simple Image Gallery Built With React And Firebase !!! <br/>
+       So,Have Fun Doodling Your Memories :) </p>
+    </div>
+  )
+}
+
+export default Title;
